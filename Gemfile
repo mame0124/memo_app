8807b1bcd4f0deda9b3e8b5496cab2063468ca6a
mame0124 <mame0124.rkn@gmail.com>
2022-09-06 20:16:34 +0900

    rubocopとERB lintを通した

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "sinatra"
-gem "webrick"
+gem 'erb_lint', require: false
+gem 'sinatra'
 gem 'sinatra-contrib'
-
+gem 'webrick'
--- memoapp.rb
+++ memoapp.rb
@@ -1,75 +1,74 @@
-require "sinatra"
-require "sinatra/reloader"
-require "json"
-require "SecureRandom"
+# frozen_string_literal: true
 
-get "/memos" do
-  @memo_infos = IO.readlines('memo.json').map {|memo_info| JSON.parse(memo_info, symbolize_names: true)}
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require 'SecureRandom'
+
+get '/memos' do
+  @memo_infos = IO.readlines('memo.json').map { |memo_info| JSON.parse(memo_info, symbolize_names: true) }
   erb :index
 end
 
-get "/memos/new" do
+get '/memos/new' do
   erb :new
 end
 
-get "/memos/:id" do
+get '/memos/:id' do
   @id = params[:id]
   @title = ''
   @memo = ''
-  IO.readlines('memo.json').each {|memo_info|
+  IO.readlines('memo.json').each do |memo_info|
     if JSON.parse(memo_info, symbolize_names: true)[:id] == @id
       @title = JSON.parse(memo_info, symbolize_names: true)[:title]
       @memo = JSON.parse(memo_info, symbolize_names: true)[:memo].gsub(/\n|\r\n|\r/, '<br>')
     end
-  }
+  end
   erb :show
 end
 
-get "/memos/:id/edit" do
+get '/memos/:id/edit' do
   @id = params[:id]
   @title = ''
   @memo = ''
-  IO.readlines('memo.json').each {|memo_info|
+  IO.readlines('memo.json').each do |memo_info|
     if JSON.parse(memo_info, symbolize_names: true)[:id] == @id
       @title = JSON.parse(memo_info, symbolize_names: true)[:title]
       @memo = JSON.parse(memo_info, symbolize_names: true)[:memo]
     end
-  }
+  end
   erb :edit
 end
 
-post "/memos" do
+post '/memos' do
   new_memo_info = params
-  unless new_memo_info["title"].match?(/[^ ]+/)
-    new_memo_info["title"] = "No title"
-  end
-  new_memo_info["id"] = SecureRandom.hex(5)
-  IO.write('memo.json', "#{new_memo_info.to_json}\n", mode: "a")
+  new_memo_info['title'] = 'No title' unless new_memo_info['title'].match?(/[^ ]+/)
+  new_memo_info['id'] = SecureRandom.hex(5)
+  IO.write('memo.json', "#{new_memo_info.to_json}\n", mode: 'a')
   redirect 'http://localhost:4567/memos'
 end
 
-delete "/memos/:id" do
+delete '/memos/:id' do
   json = []
-  IO.readlines('memo.json').each {|memo_info|
+  IO.readlines('memo.json').each do |memo_info|
     if JSON.parse(memo_info, symbolize_names: true)[:id] != params[:id]
-      json << JSON.parse(memo_info,symbolize_names: true)
-    end
-  }
-  IO.write('memo.json',json.map{|x| "#{x.to_json}\n"}.join )
-  redirect 'http://localhost:4567/memos'
-end
-
-patch "/memos/:id" do
-  json = []
-  IO.readlines('memo.json').each {|memo_info|
-    if JSON.parse(memo_info, symbolize_names: true)[:id] == params[:id]
-      params.delete("_method")
-      json << params
-    elsif
       json << JSON.parse(memo_info, symbolize_names: true)
     end
-  }
-  IO.write('memo.json',json.map{|x| "#{x.to_json}\n"}.join ) 
+  end
+  IO.write('memo.json', json.map { |x| "#{x.to_json}\n" }.join)
   redirect 'http://localhost:4567/memos'
 end
 
+patch '/memos/:id' do
+  json = []
+  IO.readlines('memo.json').each do |memo_info|
+    if JSON.parse(memo_info, symbolize_names: true)[:id] == params[:id]
+      params.delete('_method')
+      json << params
+    else
+      json << JSON.parse(memo_info, symbolize_names: true)
+    end
+  end
+  IO.write('memo.json', json.map { |x| "#{x.to_json}\n" }.join)
+  redirect 'http://localhost:4567/memos'
+end
